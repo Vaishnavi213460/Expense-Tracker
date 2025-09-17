31aaff820462708a5045b5de9e43c146d4b7ac65
Vaishnavi <vaishnavi@adhoonik.com>
2025-09-17 12:46:25 +0530

    pie_chart added

--- src/components/SummaryCard/SummaryCard.js
+++ src/components/SummaryCard/SummaryCard.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { FaPlus } from 'react-icons/fa';
 import './SummaryCard.css';
 
 const SummaryCard = ({ title, amount, buttonText, onAddClick, isIncomeCard }) => {
-  const cardClassName = `card ${isIncomeCard ? 'balance-card' : 'expense-card'}`;
-  const amountClassName = `amount ${isIncomeCard ? 'green-text' : 'orange-text'}`;
-
   return (
-    <div className={cardClassName}>
-      <h2>{title}: <span className={amountClassName}>₹{amount.toLocaleString()}</span></h2>
-      <button
-        type="button"
-        className={`btn ${isIncomeCard ? 'green' : 'red'}`}
+    <div className="summary-card">
+      <h3>{title}: 
+        <span className={isIncomeCard ? 'amount income' : 'amount expense'}>
+          ₹{amount}
+        </span>
+      </h3>
+      <button 
+        className={isIncomeCard ? 'btn green' : 'btn red'} 
         onClick={onAddClick}
       >
-        <FaPlus /> &nbsp; {buttonText}
+        {buttonText}
       </button>
     </div>
   );
